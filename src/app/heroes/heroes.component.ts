ef6c96cca1f6dc1c80e138891e60cdcfe0983656
Chang Zhao <t-chaz@microsoft.com>
2019-07-24 16:46:59 +0800

    chapter 2 completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
-@Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+@Component({ // component装饰器函数，为该组件指定元数据
+  selector: 'app-heroes', // 组件选择器
+  templateUrl: './heroes.component.html', // 组件模板文件位置
+  styleUrls: ['./heroes.component.css'] // 私有CCS样式表位置
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent implements OnInit { 
 
   constructor() { }
 
-  ngOnInit() {
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  
+  ngOnInit() { // life cycle hook, 初始化逻辑
   }
 
 }
